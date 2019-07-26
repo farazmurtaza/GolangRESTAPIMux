25b3dcbdbc5522da90f3bb5310c9362b7ae998fa
farazmurtaza <farazmemon.16@gmail.com>
2019-07-27 03:38:46 +0500

    implemented routes

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	// "encoding/json"
@@ -17,7 +19,7 @@ import (
 	// "github.com/gorilla/mux"
 )
 
-//book struct (basically the model)
+//Book struct (basically the model)
 type Book struct {
 	ID     string  `json:"id"`
 	Isbn   string  `json:"isbn"`
@@ -25,44 +27,93 @@ type Book struct {
 	Author *Author `json:"author"`
 }
 
-//author struct
+//Author struct
 type Author struct {
-	FirstName string `json"firstname"`
-	LastName  string `json"lastname"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
 }
+
+//arrays need defined length, slice is basically variable length array
+
+//init books var as a slice Book struct
+var books []Book
 
 //every function that we create for route handler has to have these two parameters. this is similar
 // to the callback in app.get('/', (req, res)) in express
 
 //get all books @GET
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("api hit")
+	// fmt.Println("api hit")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 //get single book @GET
 func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //Get params
 
+	//loop through books and find ID
+
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 // create a new book @POST
 func createBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(1000000)) //may repeat but wth
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 // update an existing book @PUT
 func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
 
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 // delete an existing book @DELETE
 func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
 
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func main() {
 
 	//init router
 	r := mux.NewRouter()
+
+	books = append(books, Book{ID: "1", Isbn: "87687", Title: "Monk Who Sold His Ferrari", Author: &Author{FirstName: "John", LastName: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "00687", Title: "Monk Who Did Not Sell His Ferrari", Author: &Author{FirstName: "Johnny", LastName: "Depp"}})
 
 	// create route handlers which will establish endpoints
 
